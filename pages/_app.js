dbc41eb2119ff348d6eb93dbd1cc995ea04e9e1c
Vrunda Joshi <vrunda1002@gmail.com>
2024-07-04 17:51:58 +0530

    default files - _app and _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
+import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({Component, pageProps}) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS events' />
+        <meta name='viewport' content='inital-scale=1.0,width=device-width' />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
